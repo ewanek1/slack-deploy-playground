d205dad131518cf836040636e31cae840a20221b
Eva Wanek <ewanek@slack-corp.com>
2025-08-14 13:25:45 -0700

    Added logger

--- src/logger.js
+++ src/logger.js
@@ -1,20 +1,14 @@
-import { LogLevel } from "@slack/logger";
+const { LogLevel } = require("@slack/logger");
 
 /**
  * The Logger class creates a Logger to output debug messages and errors.
  *
  * @see {@link https://tools.slack.dev/node-slack-sdk/web-api/#logging}
  */
-export default class Logger {
+class Logger {
   /**
    * The logger for outputs.
    * @type {import("@slack/logger").Logger}
-   */
-  logger;
-
-  /**
-   * Shared utilities specific to the GitHub action workflow.
-   * @param {import("@actions/core")} core - GitHub Actions core utilities.
    */
   constructor(core) {
     this.logger = {
@@ -30,3 +24,5 @@ export default class Logger {
     };
   }
 }
+
+module.exports = Logger;
